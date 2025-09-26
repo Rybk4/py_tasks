163a6935a58ee11b5c7a19e170bca67fc16f7f4d
Rybka <112930666+Rybk4@users.noreply.github.com>
2025-09-26 20:14:09 +0500

    T1 - Базовый калькулятор с операциями + и  -

---
+++ main.py
@@ -0,0 +1,28 @@
+while True:
+    a = input("Введите первое число (или 'exit' для выхода): ")
+    if a.lower() == "exit":
+        print("Калькулятор завершил работу.")
+        break
+
+    b = input("Введите второе число (или 'exit' для выхода): ")
+    if b.lower() == "exit":
+        print("Калькулятор завершил работу.")
+        break
+
+    op = input("Введите операцию (+ или -), или 'exit' для выхода: ")
+    if op.lower() == "exit":
+        print("Калькулятор завершил работу.")
+        break
+
+    try:
+        a = float(a)
+        b = float(b)
+
+        if op == "+":
+            print("Результат:", a + b)
+        elif op == "-":
+            print("Результат:", a - b)
+        else:
+            print("Ошибка: допустимы только '+' и '-'.")
+    except ValueError:
+        print("Ошибка: нужно вводить числа!")
